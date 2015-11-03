49d71a8e9c81f67b08898d29b413978df210883a
Marcel Jackwerth <marceljackwerth@gmail.com>
2015-11-03 15:23:51 +0100

    Prevent flood of notifications (fixes #12)

--- src/io/github/sirlantis/rubymine/rubocop/utils/NotifyUtil.kt
+++ src/io/github/sirlantis/rubymine/rubocop/utils/NotifyUtil.kt
@@ -3,12 +3,22 @@ package io.github.sirlantis.rubymine.rubocop.utils
 import com.intellij.notification.*
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindowId
+import javax.swing.event.HyperlinkEvent
 
 class NotifyUtil {
+    class Listener(): NotificationListener {
+        override fun hyperlinkUpdate(notification: Notification, event: HyperlinkEvent) {
+            if (event.eventType == HyperlinkEvent.EventType.ACTIVATED) {
+                // TODO
+            }
+        }
+    }
+
     companion object {
         private val TOOLWINDOW_NOTIFICATION = NotificationGroup.toolWindowGroup("RuboCop Errors", ToolWindowId.MESSAGES_WINDOW, true)
         private val STICKY_NOTIFICATION = NotificationGroup("RuboCop Errors", NotificationDisplayType.STICKY_BALLOON, true)
         private val BALLOON_NOTIFICATION = NotificationGroup("RuboCop Notifications", NotificationDisplayType.BALLOON, true)
+        private val SHOWN_NOTIFICATIONS = hashMapOf<NotificationGroup, Notification>()
 
         public fun notifySuccess(project: Project, title: String, message: String) {
             notify(NotificationType.INFORMATION, BALLOON_NOTIFICATION, project, title, message)
@@ -28,7 +38,19 @@ class NotifyUtil {
         }
 
         private fun notify(notificationType: NotificationType, group: NotificationGroup, project: Project, title: String, message: String) {
-            group.createNotification(title, message, notificationType, null).notify(project)
+            SHOWN_NOTIFICATIONS[group]?.expire()
+
+            val listener = Listener()
+            val notification = group.createNotification(title, message, notificationType, listener)
+
+            notification.whenExpired {
+                if (SHOWN_NOTIFICATIONS[group] == notification) {
+                    SHOWN_NOTIFICATIONS.remove(group)
+                }
+            }
+
+            SHOWN_NOTIFICATIONS[group] = notification
+            notification.notify(project)
         }
     }
 }
